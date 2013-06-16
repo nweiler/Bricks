cf1036e9e79a73d9b494482553dfeeee49245685
Nick Weiler <nweiler@gmail.com>
2013-06-16 16:01:34 -0500

    Initial commit.

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import random, pygame, sys, os
 from pygame.locals import *
 
-FPS = 60
+FPS = 120
 WINDOWWIDTH = 1200
 WINDOWHEIGHT = 860
-CELLSIZE = 20
+CELLSIZE = 5
 assert WINDOWWIDTH % CELLSIZE == 0, "Window width must be a mutliple of cell size."
 assert WINDOWHEIGHT % CELLSIZE == 0, "Window height must be a multiple of cell size."
 CELLWIDTH = int(WINDOWWIDTH / CELLSIZE)
@@ -19,17 +19,13 @@ BLUE =  (0,0,255)
 
 
 WINDOW = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT), 0, 32)
-pygame.display.set_caption('Bricks')
+pygame.display.set_caption('Lines')
 WHITE = (255, 255, 255)
 BLACK = (0,0,0)
 WINDOW.fill(WHITE)
 
 pygame.init()
-bricks = []
-bricks.append((WINDOW, BLACK, (0, 0, 30, 20)))
-bricks.append((WINDOW, BLACK, (90, 100, 30, 20)))
-
-bugs = []
+lines = []
 
 
 def main():
@@ -37,21 +33,15 @@ def main():
 	pygame.event.get()
 	
 	pos = pygame.mouse.get_pos()
-
-
-	circ1 = pygame.draw.circle(WINDOW, RED, (100, 100), 100)
 	
-	
-
-
-	for index, x in enumerate(bricks):
-		pygame.draw.rect(bricks[index][0], bricks[index][1], (bricks[index][2][0], bricks[index][2][1], bricks[index][2][2], bricks[index][2][3]))
+	for index, x in enumerate(lines):
+		pygame.draw.line(lines[index][0], lines[index][1], (lines[index][2][0], lines[index][2][1]), (lines[index][3][0], lines[index][3][1]), 5)
 
 	
 	if pygame.mouse.get_pressed()[0] == True:
-		bricks.append((WINDOW, BLACK, (pos[0] - pos[0] % 30, pos[1] - pos[1] % 20, 30, 20)))
+		lines.append((WINDOW, BLACK, (0, 0), (pos[0], pos[1]), 5))
 	
-	rect1 = pygame.draw.rect(WINDOW, BLUE, (pos[0] - pos[0] % 30, pos[1]- pos[1] % 20, 30, 20))
+	line1 = pygame.draw.line(WINDOW, BLUE, (0, 0), (pos[0], pos[1]), 5)
 	
 	pygame.display.update()
 	fpsClock.tick(FPS)
